e9543d20d52df98fa4aeda4b2764d3f5aaa7b6c2
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-09-14 15:49:33 +0900

    beans -ing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Link, Route, Router, Routes } from "react-router-dom";
 import "./common.css";
 import Header from "./component/common/Header";
 import Main from "./component/common/Main";
-import CoffeeBeans from "./component/common/beans/CoffeeBeans";
+import CoffeeBeans from "./component/beans/CoffeeBeans";
 
 function App() {
   return (
