6a5d6ca8570764cf6a2c56cc02ceaf7f823fae5c
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-09-14 14:48:39 +0900

    link to beans

---
+++ src/component/common/beans/CoffeeBeans.js
@@ -0,0 +1,10 @@
+import "./coffeeBeans.css";
+const CoffeeBeans = () => {
+  return (
+    <div className="content-wrap">
+      <div className="main-wrap">원두</div>
+    </div>
+  );
+};
+
+export default CoffeeBeans;
