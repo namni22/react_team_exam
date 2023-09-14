18177be2723b5d26035a14ed99c3074a63e3575b
namni22 <142199912+namni22@users.noreply.github.com>
2023-09-14 16:03:49 +0900

    메인수정

--- src/component/common/Main.js
+++ src/component/common/Main.js
@@ -3,7 +3,7 @@ import "./main.css";
 const Main = () => {
   return (
     <div className="main-wrap">
-      <img src="img/main/main1-g.jpg"></img>;
+      <img src="img/main/main1-g.jpg"></img>
     </div>
   );
 };
