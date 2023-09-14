8bc33e350e27d675bc15f9b537f0da03374ed4b0
namni22 <142199912+namni22@users.noreply.github.com>
2023-09-14 11:43:25 +0900

    라우팅 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -7,6 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>팀프로젝트 테스트</p>
+        <Link
+          to="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="App-link"
+        ></Link>
         <a
           className="App-link"
           href="https://reactjs.org"
