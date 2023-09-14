5102cac07214510463347eb50f8b94e9c8a46d3c
namni22 <142199912+namni22@users.noreply.github.com>
2023-09-14 13:41:45 +0900

    컴포넌트 폴더생성

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>팀프로젝트 테스트</p>
-        <Link
-          to="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="App-link"
-        ></Link>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>팀프로젝트 테스트</p>
     </div>
   );
 }
